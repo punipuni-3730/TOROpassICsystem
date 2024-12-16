cdac46692c1227f404c1604d112533aa699f832a
fhrk-78 <78fhruby78@gmail.com>
2024-12-16 15:33:54 +0900

    データ永続化、決済履歴保存

---
+++ src/main/java/prj/salmon/toropassicsystem/types/SavingDataJson.java
@@ -0,0 +1,10 @@
+package prj.salmon.toropassicsystem.types;
+
+import java.util.ArrayList;
+
+public class SavingDataJson {
+    public long lastupdate;
+    public ArrayList<SavingData> data;
+
+    public SavingDataJson() {}
+}
